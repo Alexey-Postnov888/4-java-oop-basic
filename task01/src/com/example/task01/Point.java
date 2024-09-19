69c680343f362d7f2657549df4a9fb5520afd4ed
Alexey Postnov <adpostnov@yandex.ru>
2024-09-19 21:06:07 +0500

    Постнов Алексей. Задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,26 @@ public class Point {
     int x;
     int y;
 
-    void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip() {
+        int temp = x;
+
+        x = -y;
+        y = -temp;
+    }
+
+    public double distance(Point point) {
+        double a = Math.pow(x - point.x, 2);
+        double b = Math.pow(y - point.y, 2);
+
+        return Math.sqrt(a + b);
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
